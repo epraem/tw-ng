5a9b1f2f2577d574b910a4a0bc89e8cd5b1b75b4
efraim <efraimg@n-compass.biz>
2024-04-03 17:14:19 +0800

    Components implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card/card.component';
+import { InputComponent } from './components/input/input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, ButtonComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    ButtonComponent,
+    CardComponent,
+    InputComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
